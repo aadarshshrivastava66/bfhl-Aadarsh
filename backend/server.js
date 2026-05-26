d89df8fba951d14f59fdbd64dec452dfc4eafc5a
Aadarsh <aadarshshrivastava66@gmail.com>
2026-05-26 16:40:33 +0530

    deployed url add

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,40 @@
 const express = require("express");
 
-const mongoose = require(
-  "mongoose"
-);
+const mongoose = require("mongoose");
 
 const cors = require("cors");
 
 require("dotenv").config();
 
-const routes = require(
-  "./routes/ticketRoutes"
-);
+const routes = require("./routes/ticketRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://bfhl-aadarsh.netlify.app",
+  })
+);
 
 app.use(express.json());
 
 app.use("/", routes);
 
-mongoose
-  .connect(
-    process.env.MONGO_URI
-  )
-  .then(() => {
-    console.log(
-      "MongoDB Connected"
-    );
+app.get("/", (req, res) => {
+  res.send("Backend Running");
+});
 
-    app.listen(
-      process.env.PORT,
-      () => {
-        console.log(
-          `Server Running ${process.env.PORT}`
-        );
-      }
-    );
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(
+        `Server Running on ${process.env.PORT}`
+      );
+    });
   })
-  .catch((err) =>
-    console.log(err)
-  );
+  .catch((error) => {
+    console.log(error);
+  });
